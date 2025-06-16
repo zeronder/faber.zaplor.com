d9f89c8a87ab8c0da2b14a159a42c108f094c3f3
zeronder <rohitsaini838797@gmail.com>
2025-06-16 17:05:56 +0530

    first index.html

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+'''
+@main.route('/about')
+def about():
+    return render_template('about.html')
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html')
+'''
